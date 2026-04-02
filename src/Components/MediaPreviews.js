66f15dcef80cc0bd2e4f72939a705204c5442d86
Marqed <Derpexsplosion@gmail.com>
2026-04-02 10:38:11 -0400

    Working on MediaPreview Opal Dropdown Arrow

--- src/Components/MediaPreviews.js
+++ src/Components/MediaPreviews.js
@@ -4,6 +4,7 @@ import HomeShorts from "./HomeShorts"
 
 import ShowContentInactive from "../Resources/art/Background Project/Gradients/Preview Content Inactive.gif"
 import ShowContentActive from "../Resources/art/Background Project/Gradients/Hide Content Active.gif"
+import DownArrow from "../Resources/art/Background Project/Gradients/Down Arrow Orange Marble Gradient.gif"
 
 const MediaPreviews = () => {
     const [contentOpen, setContentOpen] = useState(false);
@@ -29,14 +30,14 @@ const MediaPreviews = () => {
 
             {contentOpen && (
                 <div className="ContentDropdownRow">
+                    <img src={DownArrow} className="dropdown-down-arrow-one"></img>
                     <HomeEdits
                         isOpen={openSection === "edits"}
-                        onToggle={() => setOpenSection(prev => prev === "edits" ? null : "edits")}
-                    />
+                        onToggle={() => setOpenSection(prev => prev === "edits" ? null : "edits")}/>
+                    <img src={DownArrow} className="dropdown-down-arrow-two"></img>
                     <HomeShorts
                         isOpen={openSection === "shorts"}
-                        onToggle={() => setOpenSection(prev => prev === "shorts" ? null : "shorts")}
-                    />
+                        onToggle={() => setOpenSection(prev => prev === "shorts" ? null : "shorts")}/>
                 </div>
             )}
         </div>
