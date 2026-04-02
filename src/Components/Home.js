133be90d90e3f331f02aaa58a7e67e43211a7325
Marqed <Derpexsplosion@gmail.com>
2026-04-02 16:54:58 -0400

    Landing Page Content Additions

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 
 import bannerbackground from "../Resources/art/Background Project/Gradients/Orange Glob Marble.gif"
 import introheader from "../Resources/art/Background Project/Gradients/Name Header.gif"
+import startheader from "../Resources/art/Background Project/Gradients/Start Header.gif"
 import Navbar from "./Navbar.js"
 import MediaPreviews from "./MediaPreviews.js"
 
@@ -21,13 +22,25 @@ const Home = () => {
         <div className="home-name-text-container">
           <img 
             src={introheader}
-            alt="Hey, I'm Zachery / Marqed"
+            alt="hey"
             className="intro-header"
           />
           <p>
-            Content creator and developer based out of Brooklyn, New York. <br />
-            I make YouTube edits, shorts, mods, apps, websites, and much more! <br />
+            Content creator and developer based out of Brooklyn, New York. <br/>
+            I make YouTube edits, shorts, mods, apps, websites, and much more! <br/>
             This is my corner of the internet — have a look around.
+          </p>
+
+          <img 
+            src={startheader}
+            alt="start"
+            className="start-header"
+          />
+          <p>
+            Welcome to my personal site — a space where I share my work, <br/>
+            explore ideas, and highlight the skills I bring to creative projects. <br/>
+            If you're hiring or interested in collaborating with me as a freelancer, <br/>
+            be sure to check out the details page for more information. <br/>
           </p>
         </div>
       </div>
